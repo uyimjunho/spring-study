0901f92729c29e59376876fddb40d2d09204d205
uyimjunho <joonno94@kookmin.ac.kr>
2022-02-19 22:37:12 +0900

    싱글톤 순수자바로 구현 및 스프링 컨테이너 확인

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,9 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SingletonTest {
 
@@ -21,5 +24,34 @@ public class SingletonTest {
         System.out.println(memberService2);
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService instance = SingletonService.getInstance();
+        SingletonService instance1 = SingletonService.getInstance();
+
+        System.out.println(instance);
+        System.out.println(instance1);
+
+        assertThat(instance).isSameAs(instance1);
+
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+//        MemberService memberService2 = appConfig.memberService();
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
