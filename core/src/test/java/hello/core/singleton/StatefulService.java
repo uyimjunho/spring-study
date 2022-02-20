23c7802a30fb6853b58ce41543204df1cafa5fa0
uyimjunho <joonno94@kookmin.ac.kr>
2022-02-20 11:15:38 +0900

    stateless로 설계해야 하는 이유

---
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,20 @@
+package hello.core.singleton;
+
+public class StatefulService {
+
+//    private int price; //상태를 유지하는 필드
+
+//    public void order(String name, int price) { //stateful!
+//        System.out.println("name = " + name + " price = " + price);
+//        this.price = price;
+//    }
+
+    public int order(String name, int price) {//stateless;
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }
+//
+//    public int getPrice() {
+//        return price;
+//    }
+}
---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulSerivceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService = ac.getBean(StatefulService.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+
+        statefulService.order("userA", 10000);
+        statefulService1.order("userB", 20000);
+
+        int price = statefulService.getPrice();
+        System.out.println(price);
+        assertThat(statefulService.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
